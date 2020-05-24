16081c078c5b61fda2f1c098a7c8056235289cc3
PrettyKittie <34041424+PrettyKittie@users.noreply.github.com>
2020-05-23 22:53:34 -0400

    Create iptables.php

---
+++ knowledge/tutorials/iptables.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SpoonFed</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../../css/foundation.css" />
+        <link rel="stylesheet" type="text/css" href="../../css/spoon.css" />
+    </head>
+    <body>
+		<div class="base"></div>
+	    <div id="header">
+			<?php include("../../header.php"); ?>
+	    </div>
+
+	    <div id="content">
+			<div class="row">
+				<div class="small-24 medium-14 large-16 medium-push-4 large-push-4 columns">
+					<h1><a name="top" id="top"></a>Hardening SSH:</h1>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="small-24 medium-5 large-4 columns">
+					<?php include("../../leftside.php"); ?>
+				</div>
+				<div class="small-24 medium-19 large-14 columns">
+					<div class="layer">
+						
+						<section>
+							<h4>Overview:</h4>
+							<ol start="0">
+								<li><a href="#0">Intention</a></li>
+								<li><a href="#1">Description</a></li>
+								<li><a href="#2">Commands</a></li>
+								<li><a href="#3">Connection States</a></li>
+								<li><a href="#4">Persistence</a></li>
+								<li><a href="#5">Examples</a></li>
+								<li><a href="#6">Additional Options</a></li>
+                <li><a href="#7">Closing Thoughts</a></li>
+                <li><a href="#8">Additional Resources &amp; Recommended Reading</a></li>
+							</ol>					
+						</section>
+						
+						<section>
+							<a class="anchor" name="0" id="0"></a>
+							<h4>0)  Introduction & Intention</h4>
+							<p>This write-up is intended to be a simple walkthrough for linux netfilter, and will be written more like a cheatsheet than a tutorial.  It'll cover a basic explanation of netfilter terminology, using basic iptables commands for configuring netfilter, some example rules, and a few additional capabilities. </p>
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="1" id="1"></a>
+							<h4>1) Description</h4>
+							<p>Explanation of netfilter,and iptables vs ip6tables</p>
+								<pre> $$$$$$$$$</pre>
+							<p>what are chains</p>
+			
+							<p>actions</p>
+              <p>default policies</p>
+              <p>basic structure of a command</p>
+              
+              
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="2" id="2"></a>
+							<h4>2) IPTables Commands</h4>				
+							<p>$$$$$$$$$</p>
+								<pre> 
+$$$$$$$$$
+								</pre>
+              <p>$$$$$$$$$</p>
+								<ul>
+									<li>$$$$$$$$$</li>
+										<pre> $$$$$$$$$ </pre>
+								</ul>
+                
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="3" id="3"></a>
+							<h4>3) Connection States</h4>		
+							<p>$$$$$$$$$</p>
+							<p>$$$$$$$$$</p>
+              
+							<a class="back-to-top" href="#top">Back to top</a>										
+						</section>
+						
+						<section>
+							<a class="anchor" name="4" id="4"></a>
+							<h4>4) Making Rulesets Persistent</h4>
+							<pre> $$$$$$$$$ </pre> 
+							<a class="back-to-top" href="#top">Back to top</a>
+				
+						</section>
+
+						<section>
+							<a class="anchor" name="5" id="5"></a>
+							<h4>5) Example Rulesets</h4>
+							<pre> $$$$$$$$$ </pre> 
+							<a class="back-to-top" href="#top">Back to top</a>
+				
+						</section>
+            
+						<section>
+							<a class="anchor" name="6" id="6"></a>
+							<h4>6) Additional Options</h4>
+              <p> Brute-Force Protection</p>
+              <p> Ban by Geolocation </p>
+              <p> Fail2Ban </p>
+							<a class="back-to-top" href="#top">Back to top</a>
+				
+						</section>            
+
+						<section>
+							<a class="anchor" name="7" id="7"></a>
+							<h4>7) Closing Thoughts</h4>
+							<p> This cheatsheet should hopefully give you a solid understanding of what the linux netfilter is, and how to use the iptables command for basic configuration.  It is important to always keep in mind that your needs and your environment will determine which, if any, of these recommendations work for you.  You should always keep your threat model in mind when implementing configuration changes, and never blindly follow advice on the internet.  Remember: Trust, but verify!  Do you think I missed something?  Did a step not work?  Could this simply be the most bestest article you've ever seen on the interwebz?  Feel free to stop by the <a href="http://www.spoonfed.info/index.php">channel</a> and let us know what you think.  You can send your flames or flattery for this article to PrettyKittie via <a href="http://www.spoonfed.info/index.php" target="_blank">IRC</a> or <a href="http://www.spoonfed.info/whoami.php" target="_blank">email.</a> </p>
+							<a class="back-to-top" href="#top">Back to top</a>
+              
+            </section>
+
+						<section>
+							<a class="anchor" name="8" id="8"></a>
+							<h4>8) Additional Resources &amp; Recommended Reading</h4>
+							<ul>
+								<li>The #NetFilter IRC channel on Freenode</li>
+								<li><a href="https://blog.thermicorp.de/?p=12" target="_blank">Additional NetFilter links and tutorials</a></li>
+                <li><a href="$$$$$$$$$" target="_blank">$$$$$$$$$</a></li>            
+							</ul>
+						</section>
+						
+					</div>
+				</div>
+				<div class="small-24 medium-24 large-6 columns">
+					<?php include("../../rightside.php"); ?>
+				</div>
+			</div>
+	    </div>
+
+		<?php include("../../footer.php"); ?>
+	    
+    </body>
+</html>
