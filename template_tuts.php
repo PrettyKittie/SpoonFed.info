0f98dde28c93b1785e1a172bafffae606461f3c1
PrettyKittie <34041424+PrettyKittie@users.noreply.github.com>
2020-05-23 20:21:57 -0400

    Create template_tuts.php

---
+++ template_tuts.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SpoonFed</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../../css/foundation.css" />
+        <link rel="stylesheet" type="text/css" href="../../css/spoon.css" />
+    </head>
+    <body>
+		<div class="base"></div>
+	    <div id="header">
+			<?php include("../../header.php"); ?>
+	    </div>
+
+	    <div id="content">
+			<div class="row">
+				<div class="small-24 medium-14 large-16 medium-push-4 large-push-4 columns">
+					<h1><a name="top" id="top"></a>Hardening SSH:</h1>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="small-24 medium-5 large-4 columns">
+					<?php include("../../leftside.php"); ?>
+				</div>
+				<div class="small-24 medium-19 large-14 columns">
+					<div class="layer">
+						
+						<section>
+							<h4>Overview:</h4>
+							<ol start="0">
+								<li><a href="#0">Intention</a></li>
+								<li><a href="#1">$$$$$$$$$ Key</a></li>
+								<li><a href="#2">$$$$$$$$$/a></li>
+								<li><a href="#3">$$$$$$$$$</a></li>
+								<li><a href="#4">$$$$$$$$$</a></li>
+								<li><a href="#5">Closing Thoughts</a></li>
+                <li><a href="#6">Additional Resources &amp; Recommended Reading</a></li>
+							</ol>					
+						</section>
+						
+						<section>
+							<a class="anchor" name="0" id="0"></a>
+							<h4>0)  Introduction & Intention</h4>
+							<p>$$$$$$$$$This write-up is intended to be a walkthrough for configuring and hardening SSH.  I'll be configuring OpenSSH on a Debian box hosted by Digital Ocean.  While there are many other great options out there, this is simply my preferred platform and should provide some fairly solid guidelines for most distributions.$$$$$$$$$</p>
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="1" id="1"></a>
+							<h4>1)  $$$$$$$$$</h4>
+							<p>I$$$$$$$$$</p>
+								<pre> $$$$$$$$$</pre>
+							<p>$$$$$$$$$</p>
+			
+							<p>$$$$$$$$$</p>
+              
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="2" id="2"></a>
+							<h4>2)  $$$$$$$$$</h4>				
+							<p>$$$$$$$$$</p>
+								<pre> 
+$$$$$$$$$
+								</pre>
+              <p>$$$$$$$$$</p>
+								<ul>
+									<li>$$$$$$$$$</li>
+										<pre> $$$$$$$$$ </pre>
+								</ul>
+                
+							<a class="back-to-top" href="#top">Back to top</a>				
+						</section>
+						
+						<section>
+							<a class="anchor" name="3" id="3"></a>
+							<h4>3)  $$$$$$$$$</h4>		
+							<p>$$$$$$$$$</p>
+							<p>$$$$$$$$$</p>
+              
+							<a class="back-to-top" href="#top">Back to top</a>										
+						</section>
+						
+						<section>
+							<a class="anchor" name="4" id="4"></a>
+							<h4>4) $$$$$$$$$</h4>
+							<pre> $$$$$$$$$ </pre> 
+							<a class="back-to-top" href="#top">Back to top</a>
+				
+						</section>
+						
+						<section>
+							<a class="anchor" name="5" id="5"></a>
+							<h4>5) Closing Thoughts</h4>
+							<p> $$$$$$$$$This breakdown should hopefully give you a solid understanding of the common configurations options used to harden an SSH server.  These are the settings that I recommend, although it is important to keep in mind that your needs and your environment will determine which, if any, of these recommendations work for you.  You should always keep your threat model in mind when implementing configuration changes, and never blindly follow advice on the internet.  Remember: Trust, but verify!  Do you think I missed something?  Did a step not work?  Could this simply be the most bestest article you've ever seen on the interwebz?  Feel free to stop by the <a href="http://www.spoonfed.info/index.php">channel</a> and let us know what you think.  You can send your flames or flattery for this article to PrettyKittie via <a href="http://www.spoonfed.info/index.php" target="_blank">IRC</a> or <a href="http://www.spoonfed.info/whoami.php" target="_blank">email.</a> </p>
+							<a class="back-to-top" href="#top">Back to top</a>
+
+							<h4>6) Additional Resources &amp; Recommended Reading</h4>
+							<ul>
+								<li><a href="$$$$$$$$$" target="_blank">$$$$$$$$$</a></li>
+							</ul>
+						</section>
+						
+					</div>
+				</div>
+				<div class="small-24 medium-24 large-6 columns">
+					<?php include("../../rightside.php"); ?>
+				</div>
+			</div>
+	    </div>
+
+		<?php include("../../footer.php"); ?>
+	    
+    </body>
+</html>
